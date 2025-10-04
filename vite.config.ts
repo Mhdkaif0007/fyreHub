000013ab4fc5360549fd3e4f19187ee88ec254ae
kaif <mhdkaif0007@gmail.com>
2025-10-05 01:42:03 +0530

    fixing github pages issue

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
     base: '/fyreHub/',
   plugins: [react()],
   build: {
-    outDir: 'build', // Specify the output directory
+    outDir: 'dist', // Specify the output directory
   },
 })
